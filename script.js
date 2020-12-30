562db72dddbdf9d2209ff4f931ffa2ce187d263f
Tim Thornton <timothythornton8@gmail.com>
2020-12-30 14:42:06 +0000

    Animate header bar

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function expandBar() {
       x.className = "nav";
     }
   }
+
+var state = 0;
+window.onscroll = function () { 
+  "use strict";
+
+  var navBar = document.getElementById("top");
+
+  if (state == 0) {
+    var th = 25;
+  }
+  else {
+    var th = 0;
+  }
+
+  if (window.scrollY > th){
+    // console.log(navBar.classList);
+    navBar.classList.add("_top_scroll");
+    navBar.classList.remove("_top");
+    var state = 1;
+  } 
+  else {
+    // console.log(navBar.classList);
+    navBar.classList.add("_top");
+    navBar.classList.remove("_top_scroll");
+    var state = 0;
+  }
+};
