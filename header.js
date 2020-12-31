058d14cea3464a4b06b0d1b7d419ba3c5a936098
Tim Thornton <timothythornton8@gmail.com>
2020-12-31 22:24:18 +0000

    Common header

---
+++ header.js
@@ -0,0 +1,21 @@
+document.write(`
+    <div class = "_top" id = "top">
+        <div class = "_heading">
+            <h1 >GradZ</h1>
+            <p>Supporting students to develop into experts and showcasing potential. <br>
+            Providing companies with the platform to engage with emerging talent and fresh ideas.</p>
+        </div>
+
+        <div class = "_nav" id = "_navBar">
+            <ul>
+                <li><a href="index.html"        id = "home">Home</a></li>
+                <li><a href="about.html"        id = "about">About Us</a></li>
+                <li><a href="students.html"     id = "students">Students</a></li>
+                <li><a href="companies.html"    id = "companies">Companies</a></li>
+                <li><a href="projects.html"     id = "projects">Projects</a></li>
+                <li><a href="news.html"         id = "news">News</a></li>
+                <li><a href="javascript:void(0);" class="icon" onclick="expandBar()"><i class="fa fa-bars"></i></a></li>
+            </ul>
+        </div>
+    </div>
+`);
