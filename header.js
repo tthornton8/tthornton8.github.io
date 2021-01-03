a55d27a0f34faede9691147540a59331b6b93918
Tim Thornton <timothythornton8@gmail.com>
2021-01-03 02:06:48 +0000

    Add profile page and login bar placeholder

--- header.js
+++ header.js
@@ -6,14 +6,21 @@ document.write(`
             Providing companies with the platform to engage with emerging talent and fresh ideas.</p>
         </div>
 
+        <div class = "_login" id = "login">
+            <ul id = "login_list">
+                <li><a href = "profile.html">Log in</a></li>
+                <li><a href = "#">Register</a></li>
+            </ul>
+        </div>
+
         <div class = "_nav" id = "_navBar">
             <ul>
-                <li><a href="index.html"        id = "home">Home</a></li>
-                <li><a href="about.html"        id = "about">About Us</a></li>
-                <li><a href="students.html"     id = "students">Students</a></li>
-                <li><a href="companies.html"    id = "companies">Companies</a></li>
-                <li><a href="projects.html"     id = "projects">Projects</a></li>
-                <li><a href="news.html"         id = "news">News</a></li>
+                <li><a href="index.html"     id = "home"      >Home     </a></li>
+                <li><a href="about.html"     id = "about"     >About Us </a></li>
+                <li><a href="students.html"  id = "students"  >Students </a></li>
+                <li><a href="companies.html" id = "companies" >Companies</a></li>
+                <li><a href="projects.html"  id = "projects"  >Projects </a></li>
+                <li><a href="news.html"      id = "news"      >News     </a></li>
                 <li><a href="javascript:void(0);" class="icon" onclick="expandBar()"><i class="fa fa-bars"></i></a></li>
             </ul>
         </div>
