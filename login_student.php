904e57e102991f74e458d8e24793ac9e46b096b7
Tim Thornton <timothythornton8@gmail.com>
2021-01-06 22:07:06 +0000

    change body

--- login_student.php
+++ login_student.php
@@ -36,39 +36,31 @@ if (isset($_POST['submit'])) {
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="login_style.css" rel="stylesheet" type="text/css">
-  </head>
-  <body>
-
-    <h2>Login Form</h2>
-
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-      <div class="imgcontainer">
-        <img src="img_avatar2.png" alt="Avatar" class="avatar">
-      </div>
-
-      <div><?php echo $error_message; ?></div>
-
-      <div class="container">
-        <label for="usn"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="username" value="<?php echo $username;?>" required>
-
-        <label for="pwd"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" value="<?php echo $password;?>" required>
-            
-        <button type="submit">Login</button>
-        <label>
-          <input type="checkbox" checked="checked" name="remember"> Remember me
-        </label>
-      </div>
-
-      <div class="container" style="background-color:#f1f1f1">
-        <button type="button" class="cancelbtn">Cancel</button>
-        <span class="psw">Forgot <a href="#">password?</a></span>
-      </div>
-    </form>
-
-  </body>
+    <head>
+        <title>Login Form</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Login Form</h3>
+                    <?php if(!empty($error_message)) { ?>
+                        <div class="alert alert-danger"><?php echo $error_message; ?></div>
+                    <?php } ?>
+                    <form method="post">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" name="email" placeholder="Email" required />
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password" required />
+                        </div>
+                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
