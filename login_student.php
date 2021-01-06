86eddefd2a3de377b285c290e10a6a8e192badab
Tim Thornton <timothythornton8@gmail.com>
2021-01-06 19:26:43 +0000

    correct dbname

--- login_student.php
+++ login_student.php
@@ -77,7 +77,7 @@ $username = "root";
 $password = "x3ydNbLgiRZduK";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=mysql", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
