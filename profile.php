e6f9245d8d38d2640421292189db1e5443823b4c
Tim Thornton <timothythornton8@gmail.com>
2021-01-07 01:01:21 +0000

    debug

--- profile.php
+++ profile.php
@@ -14,9 +14,9 @@ if (isset($_SESSION['id'])) {
     $sql = "SELECT * FROM test_user WHERE username = '".$conn->real_escape_string($id)."'";
     $result = $conn->query($sql);
     $row = $result->fetch_assoc();
-    $name = "";//$row['name'];
-    $degree = "";//$row['degree'];
-    $uni = "";//$row['uni'];
+    $name = $row['name'];
+    $degree = $row['degree'];
+    $uni = $row['uni'];
 
 } else {
     $logged_in = 'false';
