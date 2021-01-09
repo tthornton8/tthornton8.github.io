e75d256eb589f4ba4038bf0eb53fbb81e2249335
Tim Thornton <timothythornton8@gmail.com>
2021-01-09 23:28:14 +0000

    popups

--- uploads.php
+++ uploads.php
@@ -5,42 +5,40 @@ $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($fn,PATHINFO_EXTENSION));
 $target_file = $target_dir . md5($id . basename($_FILES["fileToUpload"]["name"])) . "." . $imageFileType;
 
-echo $target_file;
-
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+    echo '<script> alert("File is an image - " . $check["mime"] . ".") </script>';
     $uploadOk = 1;
   } else {
-    echo "File is not an image.";
+    echo '<script> alert("File is not an image.") </script>';
     $uploadOk = 0;
   }
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
+  echo '<script> alert("Sorry, your file is too large.") </script>';
   $uploadOk = 0;
 }
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  echo '<script> alert("Sorry, only JPG, JPEG, PNG & GIF files are allowed.") </script>';
   $uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
+  echo <script> alert("Sorry, your file was not uploaded.") </script>';
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    echo '<script> alert("The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.") </script>';
   } else {
-    echo "Sorry, there was an error uploading your file.";
+    echo '<script> alert("Sorry, there was an error uploading your file.") </script>';
   }
 }
 ?>
