2a62171e0b86350521210bf5661e52bf5134ec52
Tim Thornton <timothythornton8@gmail.com>
2021-01-12 23:16:05 +0000

    allow setting of tinymce content

--- profile.php
+++ profile.php
@@ -390,14 +390,18 @@ function phpAlert($msg) {
             box.value = id;
             mce.innerHTML = detail;
 
-            tinymce.init({
-                    selector: 'textarea#tinymce',
-                    plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-                    toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
-                    toolbar_mode: 'floating',
-                    tinycomments_mode: 'embedded',
-                    tinycomments_author: 'Author name',
+            try {
+                tinymce.get("tinymce").setContent(detail);
+            } catch (error) {
+                tinymce.init({
+                        selector: 'textarea#tinymce',
+                        plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+                        toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
+                        toolbar_mode: 'floating',
+                        tinycomments_mode: 'embedded',
+                        tinycomments_author: 'Author name',
                 });
+            }
         }
         </script>
 
