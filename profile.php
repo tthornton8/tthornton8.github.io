2efbb5801ba2ad389d72edbd9305f749e31f2b9d
Tim Thornton <timothythornton8@gmail.com>
2021-01-13 23:44:16 +0000

    use svg-inject

--- profile.php
+++ profile.php
@@ -147,6 +147,7 @@ function phpAlert($msg) {
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-2021.css">
     <script src="https://cdn.tiny.cloud/1/184b9akoev1y38p25nmv4os4h082uhrc9copbqe6hxbwl72t/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="svg-inject.min.js"></script>
     <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" 
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" 
         crossorigin="anonymous"> -->
@@ -180,7 +181,7 @@ function phpAlert($msg) {
             <h2 style = "grid-row: 1">Top Skills</h2>
             <?php 
                 foreach ($skills as &$row) {
-                    echo "<div class = \"_bubble _gr1\">".$row["name"]."<img src = icon.php?id=".$row["icon"]." alt = \"icon\"></div>";
+                    echo "<div class = \"_bubble _gr1\">".$row["name"]."<img src = icon.php?id=".$row["icon"]." alt = \"icon\" onload=\"SVGInject(this)\"></div>";
                 }
             ?>
         </div>
