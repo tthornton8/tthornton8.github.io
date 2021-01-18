527b4ce9b9c7212cc552ed7fb0e52b0f589061ab
Tim Thornton <timothythornton8@gmail.com>
2021-01-18 22:39:20 +0000

    dropdown button

--- profile.php
+++ profile.php
@@ -283,12 +283,12 @@ function phpAlert($msg) {
                             echo "<input type=\"text\" class=\"w3-input w3-border w3-light-grey\" id=\"skills[$j][name]\" name=\"skills[$j][name]\" placeholder=\"Skill \"$i\" value = \"".htmlspecialchars($row["name"])."\"/>\n";
                             echo "<input type=\"hidden\" class=\"w3-input w3-border w3-light-grey\" id=\"skills[$j][icon]\" name=\"skills[$j][icon]\" value = \"".htmlspecialchars($row["icon"])."\"/>\n\n";
                             
-                            echo "<button class=\"dropbtn\" type=\"button\" id = \"dropbtn_$j\"><img src = icon.php?id=".$row['icon']." width = '25px', height = '25px'></button>";
-                            echo "<div class=\"dropdown-content\">";
+                            echo "<button onclick = \"toggleVis('dropdown-content_skills_$j');\" class=\"dropbtn\" type=\"button\" id = \"dropbtn_$j\"><img src = icon.php?id=".$row['icon']." width = '25px', height = '25px'></button>";
+                            echo "<div class=\"dropdown-content\" id = \"dropdown-content_skills_$j\">";
                             while ($row = $icons->fetch_assoc()) {
                                 $img_tag = "<img src = icon.php?id=".$row['ID']." width = '20px', height = '20px' style = 'margin-right: 16px;'>";
                                 $img_tag_large = "<img src = icon.php?id=".$row['ID']." width = '25px', height = '25px'>";
-                                $onclick = "\" document.getElementById('skills[$j][icon]').value = '".$row['ID']."'; document.getElementById('dropbtn_$j').innerHTML = `$img_tag_large`;\"";
+                                $onclick = "\" document.getElementById('skills[$j][icon]').value = '".$row['ID']."'; document.getElementById('dropbtn_$j').innerHTML = `$img_tag_large`; toggleVis('dropdown-content_skills_$j');\"";
                                 echo "\t\t\t\t\t\t\t\t<a onclick = $onclick id = \"icon_".$row['ID']."\">".$img_tag.$row['descrip']."</a>\n";
                             }
                             echo "</div>";
@@ -480,6 +480,15 @@ function phpAlert($msg) {
                 });
             }
         }
+
+        function toggleVis(elid) {
+            var el = document.getElementByID(elid);
+            if (el.style.display == 'none') {
+                el.style.display = 'inline-block';
+            } else {
+                el.style.display = 'none'
+            };
+        };
         </script>
 
 </body>
