f3a0b2b2f9bd0f28b5d29789241bd304a0e372f1
Tim Thornton <timothythornton8@gmail.com>
2021-01-24 17:06:07 +0000

    debug

--- profile.php
+++ profile.php
@@ -331,7 +331,7 @@ function phpAlert($msg) {
                         foreach ($icons as &$irow) {
                             $img_tag = "<img src = icon.php?id=".$irow['ID']." width = '20px', height = '20px' style = 'margin-right: 16px;'>";
                             $img_tag_large = "<img src = icon.php?id=".$irow['ID']." width = '25px', height = '25px'>";
-                            $onclick = "\" document.getElementById('skills[$j][icon]').value = '".$irow['ID']."'; document.getElementById('dropbtn_projects$j').innerHTML = `$img_tag_large`; toggleVis('dropdown-content_projects_$j');\"";
+                            $onclick = "\" document.getElementById('projects[$j][icon]').value = '".$irow['ID']."'; document.getElementById('dropbtn_projects$j').innerHTML = `$img_tag_large`; toggleVis('dropdown-content_projects_$j');\"";
                             echo "\t\t\t\t\t\t\t\t<a onclick = $onclick id = \"icon_".$j."_".$irow['ID']."\">".$img_tag.$irow['descrip']."</a>\n";
                         }
                         echo "</div>";
