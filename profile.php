3d8c21769902ea509f34770563b274eca28e7f6b
Tim Thornton <timothythornton8@gmail.com>
2021-01-24 19:29:57 +0000

    debug js

--- profile.php
+++ profile.php
@@ -381,8 +381,6 @@ function phpAlert($msg) {
                     var els = createElementFromHTML(`
                         <label class="w3-text" style = "color: #0072B5; margin-top: 10px;" for="projects[${i}][name]">Project ${i+1}</label>
                         <input type="text" class="w3-input w3-border w3-light-grey" id="projects[${i}][name]" name="projects[${i}][name]" placeholder="Project ${i+1}" value = ""/>
-                        <input type="hidden" class="w3-input w3-border w3-light-grey" id="projects[${i}][icon]" name="projects[${i}][icon]" placeholder="Project ${i+1}" value = ""/>
-                        <input type="hidden" id="projects[${i}][ID]" name="projects[${i}][ID]" value="NEW">
 
                         <button onclick = "toggleVis('dropdown-content_projects_${i}');" class="dropbtn" type="button" id = "dropbtn_projects${i}"><img src = icon.php?id=0 width = '25px', height = '25px'></button>
                         <div onclick = "editProject('NEW','',0);" class = "_edit_pencil" id = "_edit_pencil">&#x1f589;</div>
@@ -399,6 +397,8 @@ function phpAlert($msg) {
                         ?>
 
                         </div>
+                        <input type="hidden" class="w3-input w3-border w3-light-grey" id="projects[${i}][icon]" name="projects[${i}][icon]" placeholder="Project ${i+1}" value = ""/>
+                        <input type="hidden" id="projects[${i}][ID]" name="projects[${i}][ID]" value="NEW">
                         <br>
                     `);
                     for (let item of els) {
