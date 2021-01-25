6d401eea8071b1a151351d38110de040380afa4f
Tim Thornton <timothythornton8@gmail.com>
2021-01-25 19:20:50 +0000

    set new skill input box to inline

--- profile.php
+++ profile.php
@@ -367,8 +367,8 @@ function phpAlert($msg) {
                     var i = document.querySelectorAll('[id^="skills\["]').length/2;
                     skills = document.getElementById("skills_section");
                     var els = createElementFromHTML(`
-                        <label class="w3-text" style = "color: #0072B5; margin-top: 10px;" for="skills[${i}][name]">Skill ${i+1}</label>
-                        <input type="text" class="w3-input w3-border w3-light-grey" id="skills[${i}][name]" name="skills[${i}][name]" placeholder="Skill ${i+1}" value = ""/>
+                        <label class="w3-text" style = "color: #0072B5; margin-top: 10px;" for="skills[${i}][name]">Skill ${i+1}</label><br>
+                        <input type="text" style = "display: inline" class="w3-input w3-border w3-light-grey" id="skills[${i}][name]" name="skills[${i}][name]" placeholder="Skill ${i+1}" value = ""/>
                         <input type="hidden" class="w3-input w3-border w3-light-grey" id="skills[${i}][icon]" name="skills[${i}][icon]" placeholder="Skill ${i+1}" value = ""/>
 
                         <button onclick = "toggleVis('dropdown-content_skills_${i}');" class="dropbtn" type="button" id = "dropbtn_skills${i}"><img src = icon.php?id=0 width = '25px', height = '25px'></button>
@@ -382,6 +382,7 @@ function phpAlert($msg) {
                             }
                             ?>
                         </div>
+                        <br>
                     `);
                     for (let item of els) {
                         skills.appendChild(item);
