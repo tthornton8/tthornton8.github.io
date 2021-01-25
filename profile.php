2285da8d562c21369797a9dbd9f08ab4b138a284
Tim Thornton <timothythornton8@gmail.com>
2021-01-25 19:23:03 +0000

    line break on new line

--- profile.php
+++ profile.php
@@ -368,6 +368,7 @@ function phpAlert($msg) {
                     skills = document.getElementById("skills_section");
                     var els = createElementFromHTML(`
                         <label class="w3-text" style = "color: #0072B5; margin-top: 10px;" for="skills[${i}][name]">Skill ${i+1}</label>
+                        <br>
                         <input type="text" style = "display: inline" class="w3-input w3-border w3-light-grey" id="skills[${i}][name]" name="skills[${i}][name]" placeholder="Skill ${i+1}" value = ""/>
                         <input type="hidden" class="w3-input w3-border w3-light-grey" id="skills[${i}][icon]" name="skills[${i}][icon]" placeholder="Skill ${i+1}" value = ""/>
 
