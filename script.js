00399a4bf4d88d5b920c917aa40d51df340826f8
Tim Thornton <timothythornton8@gmail.com>
2021-01-31 22:59:16 +0000

    heading mobile

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function expandBar() {
     var x = document.getElementById("_navBar");
-    if (x.className === "nav") {
+    if (x.className === "_nav") {
       x.className += " responsive";
     } else {
-      x.className = "nav";
+      x.className = "_nav";
     }
   };
 
