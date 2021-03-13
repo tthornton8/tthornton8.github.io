85a0c0739baacc09b064330942722dc8280137c8
Tim Thornton <timothythornton8@gmail.com>
2021-03-13 22:49:01 +0000

    add return small pfile to students page

---
+++ return_small_profile.php
@@ -0,0 +1,21 @@
+<div>
+    <h2><?php echo $name; ?></h2>
+    <img src = "<?php echo "img.php?id={$id}"; ?>">
+    <h3 style = "grid-area: r1"> Top Skills: </h3>
+    <h4 style = "grid-area: r2"> 
+    <?php 
+        foreach ($skills as &$row) {
+            echo $prow['name']."<br/>";
+        }
+    ?>
+    </h4>
+    Skill 1<br/>Skill 2<br/>Skill 3</h4>
+    <h3 style = "grid-area: r3"> Top Projects: </h3>
+    <h4 style = "grid-area: r4">
+    <?php 
+        foreach ($projects as &$prow) {
+            echo $prow['name']."<br/>";
+        }
+    ?>
+    </h4>
+</div>
