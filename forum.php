b2f2fdc0020d8e8300186a6115b5fa4a627bc77a
Tim Thornton <timothythornton8@gmail.com>
2021-03-19 22:36:07 +0000

    add secitions

--- forum.php
+++ forum.php
@@ -1,3 +1,21 @@
 <div class = "_prof_section">
     <h1>GradCherry Forum</h1>
+    <div class="_prof_section">
+        <h2> New Ideas </h2>
+    </div>
+    <div class="_prof_section">
+        <h2> New Ideas </h2>
+    </div>
+    <div class="_prof_section">
+        <h2> New Ideas </h2>
+    </div>
+    <div class="_prof_section">
+        <h2> New Ideas </h2>
+    </div>
+    <div class="_prof_section">
+        <h2> New Ideas </h2>
+    </div>
+    <div class="_prof_section">
+        <h2> New Ideas </h2>
+    </div>
 </div>
