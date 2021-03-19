4d0606d42e8b1885862f1a915a1380bb7db60fce
Tim Thornton <timothythornton8@gmail.com>
2021-03-19 23:38:11 +0000

    forum style

--- forum.php
+++ forum.php
@@ -6,12 +6,12 @@
         <h2 style = "grid-area: _last">Last Post</h2>
     </div>
     <div class="_prof_section _forum_name">
-        <h2 class = "_name center_section"> <i class="fa fa-comments"></i> New Ideas </h2>
-        <h3 class = "_threads center_section"> 8 </h3>
-        <h3 class = "_post center_section"> 12 </h3>
-        <h4 class = "_title"> title </h4>
-        <h4 class = "_user"> user </h4>
-        <h4 class = "_time"> time </h4>
+        <h2 class = "_name center_section _nomargin"> <i class="fa fa-comments"></i> New Ideas </h2>
+        <h3 class = "_threads center_section _nomargin"> 8 </h3>
+        <h3 class = "_post center_section _nomargin"> 12 </h3>
+        <h4 class = "_title _nomargin"> title </h4>
+        <h4 class = "_user _nomargin"> user </h4>
+        <h4 class = "_time _nomargin"> time </h4>
     </div>
     <div class="_prof_section _forum_name">
         <h2> Projects Discussion </h2>
