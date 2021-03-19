60c24647571ec3be180b73eda7605278f031bd52
Tim Thornton <timothythornton8@gmail.com>
2021-03-19 23:19:46 +0000

    forum style

--- forum.php
+++ forum.php
@@ -5,7 +5,7 @@
         <h2 style = "grid-area: _posts">Posts</h2>
         <h2 style = "grid-area: _last">Last Post</h2>
     </div>
-    <div class="_prof_section">
+    <div class="_prof_section _forum_name">
         <h2> <i class="fa fa-comments"></i> New Ideas </h2>
         <h3> 8 </h3>
         <h3> 12 </h3>
@@ -15,13 +15,13 @@
             <h4> time </h4>
         </div>
     </div>
-    <div class="_prof_section">
+    <div class="_prof_section _forum_name">
         <h2> Projects Discussion </h2>
     </div>
-    <div class="_prof_section">
+    <div class="_prof_section _forum_name">
         <h2> Skills Discussion </h2>
     </div>
-    <div class="_prof_section">
+    <div class="_prof_section _forum_name">
         <h2> Mentoring </h2>
     </div>
 </div>
