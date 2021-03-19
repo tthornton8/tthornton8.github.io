31ef704d242691c154da17aa6c6177a5239387de
Tim Thornton <timothythornton8@gmail.com>
2021-03-19 23:09:51 +0000

    add placeholder elements

--- forum.php
+++ forum.php
@@ -1,8 +1,17 @@
 <div class = "_prof_section">
-    <h1>GradCherry Forum</h1>
+    <h1>Name</h1>
+    <h1>Threads</h1>
+    <h1>Posts</h1>
+    <h1>Last Post</h1>
     <div class="_prof_section">
         <h2> <i class="fa fa-comments"></i> New Ideas </h2>
-        <h3> Latest Post </h3>
+        <h3> 8 </h3>
+        <h3> 12 </h3>
+        <div class = '_latest_post'>
+            <h4> title </h4>
+            <h4> user </h4>
+            <h4> time </h4>
+        </div>
     </div>
     <div class="_prof_section">
         <h2> Projects Discussion </h2>
