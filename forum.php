5ac9eb716067693860ef586180bbcf3390a870bf
Tim Thornton <timothythornton8@gmail.com>
2021-03-21 21:57:07 +0000

    add user_name

--- forum.php
+++ forum.php
@@ -15,7 +15,7 @@ function return_forum_name($section_name, $ID, $threads, $posts, $last_title, $l
         <h3 class = "_threads center_section _nomargin"> $threads </h3>
         <h3 class = "_post center_section _nomargin"> $posts </h3>
         <h4 class = "_title _nomargin"> $last_title </h4>
-        <h4 class = "_user _nomargin"> $last_user </h4>
+        <h4 class = "_user _nomargin"> $user_name </h4>
         <h4 class = "_time _nomargin"> $dstr </h4>
     </div>
     EOT;
