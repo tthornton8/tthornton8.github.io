7ce2faed884163fa9389c788ca4fc3182d402d0a
Tim Thornton <timothythornton8@gmail.com>
2021-03-22 21:37:36 +0000

    horz bars

--- return_small_profile.php
+++ return_small_profile.php
@@ -1,6 +1,7 @@
 <div id = "miniCV_<?php echo $id; ?>" onclick="showCV(<?php echo $id; ?>);">
     <h2><?php echo $name; ?></h2>
     <img src = "<?php echo "img.php?id={$id}"; ?>">
+    <hr style = "grid-area: r0">
     <h3 style = "grid-area: r1"> Top Skills: </h3>
     <h4 style = "grid-area: r2"> 
     <?php 
@@ -9,8 +10,9 @@
         }
     ?>
     </h4>
-    <h3 style = "grid-area: r3"> Top Projects: </h3>
-    <h4 style = "grid-area: r4">
+    <hr style = "grid-area: r3">
+    <h3 style = "grid-area: r4"> Top Projects: </h3>
+    <h4 style = "grid-area: r5">
     <?php 
         foreach ($projects as &$row) {
             echo $row['name']."<br/>";
