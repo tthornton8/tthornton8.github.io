70cdaed1ce27e930c3a4a9552e9067811eb97799
Tim Thornton <timothythornton8@gmail.com>
2021-03-22 23:14:49 +0000

    close_modal

--- profile.php
+++ profile.php
@@ -604,7 +604,6 @@ function phpAlert($msg) {
         
         // Get the <span> element that closes the modal
         var span = document.getElementsByClassName("close_modal")[0];
-        var span2 = document.getElementsByClassName("close_modal")[1];
         
         // When the user clicks the button, open the modal 
         btn.onclick = function() {
@@ -616,10 +615,6 @@ function phpAlert($msg) {
           modal.style.display = "none";
         }
 
-        // When the user clicks on <span> (x), close the modal
-        span2.onclick = function() {
-          modal2.style.display = "none";
-        }
         
         // When the user clicks anywhere outside of the modal, close it
         window.onclick = function(event) {
