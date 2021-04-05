5b50893afc898e3156ad659a91d3079b438ecf3d
Tim Thornton <timothythornton8@gmail.com>
2021-04-05 18:48:28 +0100

    debug tinymce

--- profile.php
+++ profile.php
@@ -163,6 +163,7 @@ if (isset($_POST['btnsubmit'])) {
             $allowedTags='<p><strong><em><u><h1><h2><h3><h4><h5><h6><img>';
             $allowedTags.='<li><ol><ul><span><div><br/><ins><del>'; 
             $text = strip_tags(stripslashes($detail),$allowedTags);
+            echo $text;
             $proj_file_id = md5($project.$id);
 
             $f = fopen("../uploads/$proj_file_id.html", 'w');
